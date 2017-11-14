f8b9712d99ae50530ad33fed8afe3c8be2e46181
Andrew Warshaver <drew@warshaver.com>
2017-11-14 14:33:34 -0800

    forgotten file in previous commit

---
+++ feed-manager/logger.py
@@ -0,0 +1,11 @@
+import datetime
+
+def record_event(s):
+    timestamp = '{:%m-%d,%H:%M:%S}'.format(datetime.datetime.now())
+    with open("events.txt", "a") as events:
+        events.write("%s,%s\n" % (timestamp, s))
+
+def record_trade(s):
+    timestamp = '{:%m-%d,%H:%M:%S}'.format(datetime.datetime.now())
+    with open("trades.txt", "a") as trades:
+        trades.write("%s,%s\n" % (timestamp, s))
