44eba51b5775dfe72084e2af6dace2ee768afd90
Andrew Warshaver <drew@warshaver.com>
2017-11-15 11:35:18 -0800

    previous

--- lambdas/liqui-feed-handler/deploy_all.sh
+++ lambdas/liqui-feed-handler/deploy_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e -x
 
-grep "^LIQUI" ../../markets.csv | while IFS=, read -r exchange token currency
+grep ../../markets.csv | while IFS=, read -r exchange token currency
 do
   ./deploy_one.sh $exchange $token $currency
 done
