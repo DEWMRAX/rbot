b205fef7c971a172bc49fbf1fa891be0bd323c59
Andrew Warshaver <drew@warshaver.com>
2017-11-14 16:03:21 -0800

    startup script for feed manager

---
+++ feed-manager/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+cd /home/ec2-user/arbot
+
+PIDFILE=PIDFILE
+
+if [ -e $PIDFILE ]; then
+  printf "pidfile exists $PIDFILE\n"
+else
+  printf "%-50s\n" "Starting..."
+  while true; do python main.py >> log.txt 2>&1; sleep 5; done &
+  echo $! > PIDFILE
+  PID=`cat $PIDFILE`
+  if [ -z $PID ]; then
+    printf "%s\n" "Fail"
+  else
+    printf "%s %s\n" "Ok" $PID
+  fi
+fi
