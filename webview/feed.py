7bee930b5134ef8c5647f593195737f27c9311f2
Andrew Warshaver <drew@warshaver.com>
2017-11-17 14:54:06 -0800

    compute imbalance for webview

--- webview/feed.py
+++ webview/feed.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 
 table = boto3.resource('dynamodb', region_name='us-east-1').Table('orderbooks')
 
+def book_age(book):
+    return time.time() - item['timestamp'] / 1000
+
 def book_to_string(item, depth):
-    age = Decimal(time.time()) - item['timestamp'] / 1000
-    ret = "%s %s AGE: %0.3fs<br>" % (item['exchange'], item['pair'], age)
+    age = book_age(item)
+    ret = "%s %s AGE: %0.3f<br>" % (item['exchange'], item['pair'], age)
 
     for i in xrange(depth, 0, -1):
         if i-1 < len(item['asks']):
@@ -25,11 +28,110 @@ def book_to_string(item, depth):
 
     return ret
 
+def check_imbalance(bids, asks):
+
+    bids_idx = 0
+    bid_price = bids[0][0]
+    asks_idx = 0
+    ask_price = asks[0][0]
+    qty = Decimal(0)
+    total_profit = Decimal(0)
+    ret = "<br>"
+
+    ret += "best bid %0.8f @ %s<br>" % (bids[0][0], buyer.exchange)
+    ret += "best ask %0.8f @ %s<br>" % (asks[0][0], seller.exchange)
+
+    while(1):
+        if bids_idx >= len(bids) or asks_idx >= len(asks):
+            break
+
+        bid = bids[bids_idx]
+        ask = asks[asks_idx]
+
+        friction = Decimal('0.005')
+        if bid[0] <= ask[0]:
+            ret += "markets uncrossed<br>"
+            break
+
+        benefit = bid[0] - ask[0]
+        pct_benefit = benefit / bid[0]
+        ret += "benefit / friction / net : %0.8f / %0.8f / %0.8f\n" % (pct_benefit * Decimal(100), friction * Decimal(100), (pct_benefit - friction) * Decimal(100))
+
+        if pct_benefit <= friction:
+            break
+
+        bid_price = bid[0]
+        ask_price = ask[0]
+
+        if bid[1] > ask[1]:
+            qty += Decimal(ask[1])
+            bids[bids_idx] = Order(bid[0], bid[1] - ask[1])
+            profit = (pct_benefit - friction) * Decimal(ask[1]) * Decimal(1000)
+            total_profit += profit
+
+            ret += "STACKING QTY %d/%d added: %0.8f, total: %0.8f\n" % (bids_idx, asks_idx, ask[1], qty)
+            # print "STACKED PROFIT: %0.8f mBTC" % profit
+
+            asks_idx += 1
+        else:
+            qty += Decimal(bid[1])
+            asks[asks_idx] = Order(ask[0], ask[1] - bid[1])
+            profit = (pct_benefit - friction) * Decimal(bid[1]) * Decimal(1000)
+            total_profit += profit
+
+            ret += "STACKING QTY %d/%d added: %0.8f, total: %0.8f\n" % (bids_idx, asks_idx, bid[1], qty)
+            # print "STACKED PROFIT: %0.8f mBTC" % profit
+
+            bids_idx += 1
+
+    if qty > Decimal(0):
+
+        ret += "ACTIONABLE IMBALANCE of %0.8f\n" % qty
+        ret += "AVAILABLE PROFIT of %0.8f mBTC/mETH/mUSDT\n" % total_profit
+
+    return ret
+
+def best_bids(books):
+    eligible_books = filter(lambda book:book_age(book) < Decimal(120), books)
+    if len(eligible_books) == 0:
+        return None
+
+    best = eligible_books[0]
+    for book in eligible_books:
+        if len(best['bids']) == 0:
+            best = book
+        elif len(book['bids']) > 0 and book['bids'][0][0] > best['bids'][0][0]:
+            best = book
+
+    return best
+
+def best_asks(books):
+    eligible_books = filter(lambda book:book_age(book) < Decimal(120), books)
+    if len(eligible_books) == 0:
+        return None
+
+    best = eligible_books[0]
+    for book in eligible_books:
+        if len(best['asks']) == 0:
+            best = book
+        elif len(book['asks']) > 0 and book['asks'][0][0] > best['asks'][0][0]:
+            best = book
+
+    return best
+
 @app.route('/<pair>')
 def show_books(pair):
     fil = boto3.dynamodb.conditions.Key('pair').eq(pair)
+    response = table.scan(FilterExpression=fil)
+    books = response['Items']
+    while response.get('LastEvaluatedKey'):
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        books.extend(respond['Items'])
 
-    return "<br><br>".join(map(lambda item:book_to_string(item, 5), table.scan(FilterExpression=fil)['Items']))
+    bids = best_bids(books)
+    asks = best_asks(books)
+
+    return check_imbalance(bids, asks) + "<br><br>".join(map(lambda item:book_to_string(item, 5), books))
 
 if __name__ == '__main__':
   app.run()
