dc57033bca2104609fb1b40c19890b02813d023e
Andrew Warshaver <drew@warshaver.com>
2017-11-16 17:44:51 -0800

    check page for symbol

--- webview/feed.py
+++ webview/feed.py
@@ -5,10 +5,29 @@ app = Flask(__name__)
 
 table = boto3.resource('dynamodb', region_name='us-east-1').Table('orderbooks')
 
+def book_to_string(item, depth):
+    ret = "\n"
+    ret += "%s %s\n" % (item.exchange, item.pair)
+
+    for i in xrange(depth, 0, -1):
+        if i-1 < len(item.asks):
+            quote = item.asks[i-1]
+            ret += "%0.8f x %0.8f\n" % (quote[0], quote[1])
+
+    ret += "==============================\n"
+
+    for i in xrange(0, depth):
+        if i < len(item.bids):
+            quote = item.bids[i]
+            ret += "%0.8f x %0.8f\n" % (quote[0], quote[1])
+
+    return ret
+
 @app.route('/<pair>')
-def xxhello_world(pair):
+def show_books(pair):
     fil = boto3.dynamodb.conditions.Key('pair').eq(pair)
-    return str(table.scan(FilterExpression=fil))
+
+    return "/n/n".join(map(lambda item:book_to_string(item, 5), table.scan(FilterExpression=fil)['Items']))
 
 if __name__ == '__main__':
   app.run()
