bfa1b5bcd6bba95cabf81451f2af6c5f84afec32
Andrew Warshaver <drew@warshaver.com>
2017-11-16 17:53:57 -0800

    changes for html

--- webview/feed.py
+++ webview/feed.py
@@ -6,20 +6,19 @@ app = Flask(__name__)
 table = boto3.resource('dynamodb', region_name='us-east-1').Table('orderbooks')
 
 def book_to_string(item, depth):
-    ret = "\n"
-    ret += "%s %s\n" % (item.exchange, item.pair)
+    ret += "%s %s<br>" % (item['exchange'], item['pair'])
 
     for i in xrange(depth, 0, -1):
-        if i-1 < len(item.asks):
-            quote = item.asks[i-1]
-            ret += "%0.8f x %0.8f\n" % (quote[0], quote[1])
+        if i-1 < len(item['asks']):
+            quote = item['asks'][i-1]
+            ret += "%0.8f x %0.8f<br>" % (quote[0], quote[1])
 
-    ret += "==============================\n"
+    ret += "==============================<br>"
 
     for i in xrange(0, depth):
-        if i < len(item.bids):
-            quote = item.bids[i]
-            ret += "%0.8f x %0.8f\n" % (quote[0], quote[1])
+        if i < len(item['bids']):
+            quote = item['bids'][i]
+            ret += "%0.8f x %0.8f<br>" % (quote[0], quote[1])
 
     return ret
 
@@ -27,7 +26,7 @@ def book_to_string(item, depth):
 def show_books(pair):
     fil = boto3.dynamodb.conditions.Key('pair').eq(pair)
 
-    return "/n/n".join(map(lambda item:book_to_string(item, 5), table.scan(FilterExpression=fil)['Items']))
+    return "<br><br>".join(map(lambda item:book_to_string(item, 5), table.scan(FilterExpression=fil)['Items']))
 
 if __name__ == '__main__':
   app.run()
