c871cf0985cff8aa841275f10fb917da6161be8c
drewshaver <drew@warshaver.com>
2017-11-30 16:26:39 +0000

    fix

--- python-src/commands/set_one_price.py
+++ python-src/commands/set_one_price.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
+import sys
 
 MongoClient().arbot.prices.insert({'symbol':sys.argv[1], 'price':float(sys.argv[2])})
--- python-src/commands/set_one_target.py
+++ python-src/commands/set_one_target.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
+import sys
 
 MongoClient().arbot.targets.update({'symbol':sys.argv[1]}, {'$set':{'balance':sys.argv[2]}})
