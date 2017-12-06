73a6619b213452d688be2b1b4267ca50250b6cbd
drewshaver <drew@warshaver.com>
2017-12-06 03:10:38 +0000

    add print statement to set target balance command

--- python-src/commands/set_one_target.py
+++ python-src/commands/set_one_target.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
 import sys
 
+print "Old balance: %s" % MongoClient().arbot.targets.find_one({'symbol':sys.argv[1]})['balance']
+
 MongoClient().arbot.targets.update({'symbol':sys.argv[1]}, {'$set':{'balance':sys.argv[2]}})
