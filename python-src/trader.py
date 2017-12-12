2dc20bf415a62896ffaa8be0a350e92ac50e99a8
Andrew Warshaver <drew@warshaver.com>
2017-12-12 13:44:44 -0600

    fix

--- python-src/trader.py
+++ python-src/trader.py
@@ -105,7 +105,7 @@ def total_balance_incl_pending(symbol):
     confirmed = total_balance(symbol)
     target = TOTAL_TARGET_BALANCE[symbol]
 
-    if (confirmed > target or near_equals(confirmed, target, '0.05')) and symbol not in ['BTC','ETH','USDT']:
+    if (confirmed > target or near_equals(confirmed, target, '0.05')):
         return confirmed
 
     transfer = open_transfers_collection.find_one({'symbol':symbol, 'active':True})
