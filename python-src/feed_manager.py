014548a71bf02cd662f254dbf7c2962ccf1aad42
Andrew Warshaver <drew@warshaver.com>
2018-03-06 17:09:06 -0500

    update feed manager to use threads on lambda invocations

--- python-src/feed_manager.py
+++ python-src/feed_manager.py
@@ -3,13 +3,13 @@ from book import query_all
 from logger import record_event
 from collections import defaultdict
 from decimal import Decimal
+from threading import Thread
 import boto3
 import time
 
-STALE_AGE = 60
-ACTIVE_AGE = 6
-INVOKE_THROTTLE = 4
-REFRESH_RATE = 2
+STALE_AGE = 30
+ACTIVE_AGE = 1
+INVOKE_THROTTLE = 2
 
 # lambda-name => timestamp of last update
 book_age = defaultdict(lambda: 99999999)
@@ -21,34 +21,26 @@ with open('../markets.csv') as f:
 lambda_client = boto3.client('lambda')
 
 def invoke_one(market, reason, waiting_time):
-    record_event("INVOKING,%s,%s,%0.4f" % (market, reason, waiting_time))
-    lambda_client.invoke(InvocationType='Event', FunctionName=str(market))
-
-def invoke_all(markets, reason):
-    for market in markets:
-        invoke_one(str(market), reason, 0)
-
-def throttled_invoke_one(market, reason, waiting_time):
-    if time.time() > last_invoked[market] + INVOKE_THROTTLE:
+    if market.startswith('LIQUI'):
+        record_event("SKIPPING,%s" % market)
+    elif time.time() > last_invoked[market] + INVOKE_THROTTLE:
         last_invoked[market] = time.time()
-        invoke_one(market, reason, waiting_time)
+        record_event("INVOKING,%s,%s,%0.4f" % (market, reason, waiting_time))
+        t = Thread(target=lambda_client.invoke, name=market, kwargs=dict(InvocationType='Event', FunctionName=market))
+        t.start()
 
-#while True:
-#    books = query_all()
+while True:
+    books = query_all()
 
-#    for pair, pair_books in books.items():
-#        for book in pair_books:
-#            book_age[book.name] = book.age
+    for pair, pair_books in books.items():
+        for book in pair_books:
+            book_age[book.name] = book.age
+        if check_imbalance(pair_books) > Decimal(0):
+            for book in pair_books:
+                if book.age > ACTIVE_AGE:
+                    invoke_one(book.name, 'ACTIVE', book.age)
 
-#        if check_imbalance(pair_books, Decimal(80)) > Decimal(0):
-#            for book in pair_books:
-#                if book.age > ACTIVE_AGE:
-#                    throttled_invoke_one(book.name, 'ACTIVE', book.age - ACTIVE_AGE)
-
-#    for market in markets:
-#        market = market
-#        if book_age[market] > STALE_AGE:
-#           throttled_invoke_one(market, 'STALE', book_age[market] - STALE_AGE)
-
-#    record_event("SLEEPING,%s" % REFRESH_RATE)
-#    time.sleep(REFRESH_RATE)
+    for market in markets:
+        market = market
+        if book_age[market] > STALE_AGE:
+            invoke_one(market, 'STALE', book_age[market])
