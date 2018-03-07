f8a77320fc40f137e93dd9e95adb85efc0d4ff8d
Andrew Warshaver <drew@warshaver.com>
2018-03-07 10:42:23 -0500

    fix accidental running of feed manager by trader, and re add invoke_all function

--- python-src/feed_manager.py
+++ python-src/feed_manager.py
@@ -15,35 +15,40 @@ INVOKE_THROTTLE = 2
 book_age = defaultdict(lambda: 99999999)
 last_invoked = defaultdict(lambda: 0)
 
-with open('../markets.csv') as f:
-    markets = [line.strip('\n').replace(',', '-') for line in f]
-
 lambda_client = boto3.client('lambda')
 
-def invoke_one(market, reason, waiting_time):
-    if time.time() > last_invoked[market] + INVOKE_THROTTLE and not market.startswith('LIQUI'):
+def invoke_one(market, reason, waiting_time, throttle=True):
+    if (throttle and time.time() > last_invoked[market] + INVOKE_THROTTLE) and not market.startswith('LIQUI'):
         last_invoked[market] = time.time()
         record_event("INVOKING,%s,%s,%0.4f" % (market, reason, waiting_time))
         t = Thread(target=lambda_client.invoke, name=market, kwargs=dict(InvocationType='Event', FunctionName=market))
         t.start()
 
-while True:
-    record_event("QUERY_ALL_START")
-    books = query_all()
-    record_event("QUERY_ALL_END")
-
-    for pair, pair_books in books.items():
-        for book in pair_books:
-            book_age[book.name] = book.age
-        if check_imbalance(pair_books) > Decimal(0):
-            for book in pair_books:
-                if book.age > ACTIVE_AGE:
-                    invoke_one(book.name, 'ACTIVE', book.age)
-
+def invoke_all(markets, reason):
     for market in markets:
-        market = market
-        if book_age[market] > STALE_AGE:
-            invoke_one(market, 'STALE', book_age[market])
+        invoke_one(market, reason, 0, throttle=False)
 
-    record_event("SLEEPING,1")
-    time.sleep(1)
+if __name__ == '__main__':
+    with open('../markets.csv') as f:
+        all_markets = [line.strip('\n').replace(',', '-') for line in f]
+
+    while True:
+        record_event("QUERY_ALL_START")
+        books = query_all()
+        record_event("QUERY_ALL_END")
+
+        for pair, pair_books in books.items():
+            for book in pair_books:
+                book_age[book.name] = book.age
+            if check_imbalance(pair_books) > Decimal(0):
+                for book in pair_books:
+                    if book.age > ACTIVE_AGE:
+                        invoke_one(book.name, 'ACTIVE', book.age)
+
+        for market in all_markets:
+            market = market
+            if book_age[market] > STALE_AGE:
+                invoke_one(market, 'STALE', book_age[market])
+
+        record_event("SLEEPING,1")
+        time.sleep(1)
--- python-src/trader.py
+++ python-src/trader.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 import poloniex, bittrex, binance, liqui, kraken, GDAX, itbit, bitflyer, exchange
 from book import query_all, query_pair
-from feed_manager import invoke_one, invoke_all
+from feed_manager import invoke_all
 from logger import record_event, record_trade
 from order import Order
 from pair import ALL_PAIRS, ALL_SYMBOLS, pair_factory
