8ecc4d09a7237f284bf248cce7f20e75032eaf7e
Andrew Warshaver <drew@warshaver.com>
2018-05-30 13:58:01 -0400

    simply invoke all

--- python-src/feed_manager.py
+++ python-src/feed_manager.py
@@ -37,24 +37,25 @@ if __name__ == '__main__':
     with open('../markets.csv') as f:
         all_markets = [line.strip('\n').replace(',', '-') for line in f]
 
-    while True:
-        record_event("QUERY_ALL_START")
-        books = query_all()
-        record_event("QUERY_ALL_END")
-
-        for pair, pair_books in books.items():
-            for book in pair_books:
-                book_age[book.name] = book.age
-
-            if near_crossed(pair_books) or any(map(lambda sym:pair.startswith(sym), MAKER_SYMBOLS)):
-                for book in pair_books:
-                    if book.age > ACTIVE_AGE:
-                        invoke_one(book.name, 'ACTIVE', book.age)
-
-        for market in all_markets:
-            market = market
-            if book_age[market] > STALE_AGE:
-                invoke_one(market, 'STALE', book_age[market])
-
-        record_event("SLEEPING,1")
-        time.sleep(1)
+    invoke_all(all_markets, 'TEST')
+    # while True:
+    #     record_event("QUERY_ALL_START")
+    #     books = query_all()
+    #     record_event("QUERY_ALL_END")
+    #
+    #     for pair, pair_books in books.items():
+    #         for book in pair_books:
+    #             book_age[book.name] = book.age
+    #
+    #         if near_crossed(pair_books) or any(map(lambda sym:pair.startswith(sym), MAKER_SYMBOLS)):
+    #             for book in pair_books:
+    #                 if book.age > ACTIVE_AGE:
+    #                     invoke_one(book.name, 'ACTIVE', book.age)
+    #
+    #     for market in all_markets:
+    #         market = market
+    #         if book_age[market] > STALE_AGE:
+    #             invoke_one(market, 'STALE', book_age[market])
+    #
+    #     record_event("SLEEPING,1")
+    #     time.sleep(1)
