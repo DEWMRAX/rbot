ed1281c12ff675abb197d3e3d5bc7c576fcd7b6c
Andrew Warshaver <drew@warshaver.com>
2019-05-26 17:57:04 -0400

    update node runtime for aws lambda deprecation

--- lambdas/feed-handler/deploy_one.sh
+++ lambdas/feed-handler/deploy_one.sh
@@ -10,7 +10,7 @@ echo "creating lambda" $NAME
 aws lambda delete-function --function-name $NAME 2>/dev/null || echo "function non-existant"
 aws lambda create-function \
   --region us-east-1 \
-  --runtime nodejs6.10 \
+  --runtime nodejs8.10 \
   --role arn:aws:iam::554285174758:role/dynamodb-writer \
   --handler build/index.handler \
   --code S3Bucket=dewmrax-lambdas-2,S3Key=feed-handler.zip \
